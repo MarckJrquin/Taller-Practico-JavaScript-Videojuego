20fc6c06baa430fb8b712e7196062497fe885dfd
MarckJrquin <marck0208@outlook.com>
2023-04-01 23:05:56 -0500

    introduccion a canvas

--- game.js
+++ game.js
@@ -7,7 +7,11 @@ window.addEventListener('load', startGame);
 
 function startGame(){
     /* === Métodos del canvas === */
-    game.fillRect(0, 0, 100, 100); /* Lugar donde inicia el trazo. */
-    game.clearRect(0, 0, 50, 50);
+    game.fillRect(80, 30, 150, 100); /* Lugar donde inicia el trazo. */
+    game.clearRect(100, 50, 110, 60); /* Sirve como borrador para alguna parte del canvas. */
+    game.font = '12px Verdana'; /* Tamaño que tendrá el texto del fillText y tipo de fuente.*/
+    game.fillStyle = 'purple'; /* Nos permite añadir estilos CSS al fillText.*/
+    game. textAlign = 'start'; /* Posición del texto, donde comienza o donde termina respecto a la posición X y Y. */
+    game.fillText('MarckJrquin', 120, 85); /* Nos permite insertar texto dentro del canvas.*/
 
 }
