fcd84ba20c409ecc811d6561d3a19b214fb2265f
MarckJrquin <marck0208@outlook.com>
2023-04-02 00:29:17 -0500

    arreglo bidimensional

--- game.js
+++ game.js
@@ -5,29 +5,26 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
 
-function startGame(){
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-    let canvasSize = window.innerWidth * 0.6;
-
-    if (window.innerHeight > window.innerWidth){
-        canvasSize = window.innerWidth * 0.75;
-    } else {
-        canvasSize = window.innerHeight * 0.75;
-    }
-
+function setCanvasSize(){
+    canvasSize = Math.min(window.innerHeight, window.innerWidth)*0.75;
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
+    elementsSize = (canvasSize / 10) - 1;
+    startGame();
+}
 
-    const elementsSize = (canvasSize / 10) - 1;
-    
-    console.log({canvasSize, elementsSize});
-
-    game.font = elementsSize + 'px Verdana';
+function startGame(){
+    game.font = `${elementsSize}px Verdana`;
     game.textAlign = '';
     for (let i = 0; i < 10; i++){
-        game.fillText(emojis['X'], elementsSize * i, elementsSize);
+        for (let j = 1; j <= 10; j++){
+            game.fillText(emojis['X'], elementsSize * i, elementsSize * j);
+        }
     }
-    
 }
